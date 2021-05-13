7fef00ff75b985fb3fdf6bf612d78274b7ff490f
Jhean Ceballos <jheancc@gmail.com>
2021-05-13 15:53:52 -0500

    Unity version update to 2019.4.21f1

---
+++ Assets/_CustomStageElements/Scripts/CustomStageInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Synth.Utils {
+    public class CustomStageInfo : MonoBehaviour
+    {
+        public string StageName;
+        public string Author;
+        public string ExperienceTag;
+    }
+}
